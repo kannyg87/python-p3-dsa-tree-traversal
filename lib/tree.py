38eb419459d40777f88475b606ef9715da187cd3
Kanny <kanny.ghafour87@gmail.com>
2023-12-14 12:21:17 -0600

    lab completed

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        return self._dfs_search(self.root, id)
+
+    def _dfs_search(self, node, id):
+        if node is None:
+            return None
+        if node['id'] == id:
+            return node
+        for child in node['children']:
+            found = self._dfs_search(child, id)
+            if found is not None:
+                return found
+        return None
